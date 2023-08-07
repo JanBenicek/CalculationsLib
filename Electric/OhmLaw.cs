e7a17634fe0ec9ff20338ac5c4a6c03c5a2da378
Jan Beníček <jan00@benicek.xyz>
2023-08-07 11:17:45 +0200

    Lib Created V0.0.1

---
+++ Electric/OhmLaw.cs
@@ -0,0 +1,40 @@
+﻿namespace Calculations.Electric
+{
+    public class OhmLaw
+    {
+
+        /// <summary>
+        /// Calculate Resistance from Voltage and Current
+        /// </summary>
+        /// <param name="Voltage">Voltage</param>
+        /// <param name="Current">Current</param>
+        /// <returns>Resistance</returns>
+        public static decimal Resistance(decimal Voltage, decimal Current)
+        {
+            return Voltage / Current;
+        }
+
+		/// <summary>
+		/// Calculate Voltage from Resistance and Current
+		/// </summary>
+		/// <param name="Resistance">Resistance</param>
+		/// <param name="Current">Current</param>
+		/// <returns>Voltage</returns>
+		public static decimal Voltage(decimal Resistance, decimal Current)
+        {
+            return Current * Resistance;
+        }
+
+		/// <summary>
+		/// Calculate Current from Voltage and Resistance
+		/// </summary>
+		/// <param name="Voltage">RVoltage</param>
+		/// <param name="Resistance">Resistance</param>
+		/// <returns>Current</returns>
+		public static decimal Current(decimal Voltage, decimal Resistance) 
+        { 
+            return Voltage / Resistance;
+        }
+
+    }
+}
