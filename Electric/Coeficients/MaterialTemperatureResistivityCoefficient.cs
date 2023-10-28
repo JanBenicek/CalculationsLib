326a2ad5f3fbee9d6aeaec898e457d49576088c9
Jan Beníček <jan00@benicek.xyz>
2023-10-28 10:06:39 +0200

    Changet file layout

---
+++ Electric/Coeficients/MaterialTemperatureResistivityCoefficient.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculations.Electric.Coeficients
+{
+    /// <summary>
+    /// Constants of Temperature Coeficients of Resistivity
+    /// </summary>
+    public class MaterialTemperatureResistivityCoefficient
+    {
+
+        public static decimal Aluminium = 4.9E-3m;
+        public static decimal Brass50 = 2E-3m;
+        public static decimal Brass99 = 7E-3m;
+        public static decimal Constantan = -0.03E-3m;
+        public static decimal Copper = 3.92E-3m;
+        public static decimal Gold = 4E-3m;
+        public static decimal Iron = 6E-3m;
+        public static decimal CarbonGraphiteMin = -6E-3m;
+        public static decimal CarboneGraphiteMax = 1.2E-3m;
+        public static decimal Lead = 4.2E-3m;
+        public static decimal NiChrome = 0.2E-3m;
+        public static decimal Nickelin = 0.11E-3m;
+        public static decimal Platinum = 3.9E-3m;
+        public static decimal Silver = 3.8E-3m;
+        public static decimal Tin = 4.2E-3m;
+        public static decimal Wolfram = 4.1E-3m;
+
+    }
+}
